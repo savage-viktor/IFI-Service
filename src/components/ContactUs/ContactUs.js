8af40810022b2d10cda47520c30b68362ad20f8a
Viktor <93682529+savage-viktor@users.noreply.github.com>
2023-09-28 03:05:46 +0300

    footer

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -3,20 +3,38 @@ import '../ContactUs/ContactUs.css';
 function ContactUs() {
   return (
     <form class="contact-us__form">
-      <input class="contact-us__input" type="text" placeholder="Введіть імя" />
-      <input
-        class="contact-us__input"
-        type="phone"
-        placeholder="Введіть телефон"
-      />
-      <input
-        class="contact-us__input"
-        type="email"
-        placeholder="Введіть пошту"
-      />
+      <label class="contact-us__input-label">
+        <input
+          class="contact-us__input"
+          type="text"
+          placeholder="Введіть імя"
+        />
+        <svg class="contact-us__icon" width="20px" height="26px">
+          <use href="./images/icons_sprite.svg#icon-form-name"></use>
+        </svg>
+      </label>
+      <label class="contact-us__input-label">
+        <input
+          class="contact-us__input"
+          type="phone"
+          placeholder="Введіть телефон"
+        />
+        <svg class="contact-us__icon" width="16px" height="25px">
+          <use href="./images/icons_sprite.svg#icon-form-phone"></use>
+        </svg>
+      </label>
+      <label class="contact-us__input-label">
+        <input
+          class="contact-us__input"
+          type="email"
+          placeholder="Введіть пошту"
+        />
+        <svg class="contact-us__icon" width="23px" height="16px">
+          <use href="./images/icons_sprite.svg#icon-form-email"></use>
+        </svg>
+      </label>
+
       <textarea
-        // cols="30"
-        // rows="10"
         class="contact-us__comment"
         type="text"
         placeholder="Повідомлення"
