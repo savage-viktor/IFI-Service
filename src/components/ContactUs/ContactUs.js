c84b9f26fb015f9b1f09aed891c40634ac005f26
Viktor <93682529+savage-viktor@users.noreply.github.com>
2023-09-30 03:28:14 +0300

    Form

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,50 +1,18 @@
-import '../ContactUs/ContactUs.css';
+import Form from './Form/Form';
+import contactUs from '../../contactUs.json';
 
 function ContactUs() {
-  return (
-    <form class="contact-us__form">
-      <label class="contact-us__input-label">
-        <input
-          class="contact-us__input"
-          type="text"
-          placeholder="Введіть імя"
-        />
-        <svg class="contact-us__icon" width="20px" height="26px">
-          <use href="./images/icons_sprite.svg#icon-form-name"></use>
-        </svg>
-      </label>
-      <label class="contact-us__input-label">
-        <input
-          class="contact-us__input"
-          type="phone"
-          placeholder="Введіть телефон"
-        />
-        <svg class="contact-us__icon" width="16px" height="25px">
-          <use href="./images/icons_sprite.svg#icon-form-phone"></use>
-        </svg>
-      </label>
-      <label class="contact-us__input-label">
-        <input
-          class="contact-us__input"
-          type="email"
-          placeholder="Введіть пошту"
-        />
-        <svg class="contact-us__icon" width="23px" height="16px">
-          <use href="./images/icons_sprite.svg#icon-form-email"></use>
-        </svg>
-      </label>
 
-      <textarea
-        class="contact-us__comment"
-        type="text"
-        placeholder="Повідомлення"
-      ></textarea>
+  const handleSubmit = (name, phone, email, message) => {
+    contactUs.push({ name, phone, email, message });
 
-      <button class="contact-us__button" type="submit">
-        Надіслати
-      </button>
-    </form>
-  );
+    console.log('Сабміт!!!', name, phone, email, message);
+    console.log(contactUs);
+
+ 
+  };
+
+  return <Form onSubmit={handleSubmit} />;
 }
 
 export default ContactUs;
